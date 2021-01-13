1d54060a96e86e68ea9de3bcba1e7d48bbb23b9e
kaleginiene <ieva.kaleginiene@gmail.com>
2021-01-14 00:33:21 +0200

    created a products list

---
+++ src/utils/products.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    id: 1,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=6b5ee4aa-c9a0-4691-bd22-5d00a45d37e2",
+    title: "Batonėlis PULSAAR su juodaisiais serbentais, 60 g",
+    price: 1.79,
+    glutenFree: true,
+  },
+  {
+    id: 2,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=e7d9f8b9-6d5c-46b5-a7b2-50eeeb081e15",
+    title: "Vytinta dešra be nitritinės druskos PRO NATURA, a. r., 90 g",
+    price: 1.35,
+    glutenFree: false,
+  },
+  {
+    id: 3,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=925b7d93-5abe-49c8-89d4-1f46f9011111",
+    title: "Malta kava JACOBS KRONUNG, 500 g",
+    price: 2.99,
+    glutenFree: false,
+  },
+  {
+    id: 4,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=5493c79d-dde4-46ad-a617-5734bb44a194",
+    title: "WC valiklis DOMESTOS CITRUS FRESH, 750 ml",
+    price: 1.19,
+    glutenFree: false,
+  },
+  {
+    id: 5,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=16da9060-7a9b-4488-aebe-28279544f49d",
+    title: "Silpnai sūdytos silkių filė gabaliukai aliejuje su saulėgr., 200 g",
+    price: 1.25,
+    glutenFree: false,
+  },
+  {
+    id: 6,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=41df277e-3b61-493c-a161-068387c97897",
+    title: "Fermentinis sūris ATLEET ORIGINAL, 26% rieb., 200 g",
+    price: 1.32,
+    glutenFree: false,
+  },
+  {
+    id: 7,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=9d85ef40-128d-4d3d-9926-1efcf3cd9799",
+    title: "Sauskelnės-kelnaitės PAMPERS PANTS 5 (12-17 kg), 48 vnt.",
+    price: 10.99,
+    glutenFree: false,
+  },
+  {
+    id: 8,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=db7a3a05-f3fc-4b0f-857c-d230dcb1a40e",
+    title: "Skalbiamosios kapsulės ARIEL COLOR 3in1, 14 skalbimų, 14 vnt.",
+    price: 5.46,
+    glutenFree: false,
+  },
+  {
+    id: 9,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=61bf2dc3-2165-4f3f-9384-6ed8bc265e3b",
+    title: "Raudonos saldžiosios paprikos (70-90 mm), 1 kg",
+    price: 1.69,
+    glutenFree: false,
+  },
+  {
+    id: 10,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=fe89254c-ec64-46f2-b2e2-17bc5e5c5874",
+    title: "Žaliosios besėklės vynuogės, 1 kg",
+    price: 4.19,
+    glutenFree: false,
+  },
+  {
+    id: 11,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=e6dc75c0-bfbc-4ea9-9af0-635de7c045aa",
+    title: "Ekologiškas DVARO pienas, 3,5% rieb., 1 l",
+    price: 1.39,
+    glutenFree: false,
+  },
+  {
+    id: 12,
+    image:
+      "https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=6ecf96ad-624e-49f8-87b2-a8f94f7bd966",
+    title: "DVARO šaltibarščių rūgpienis, 6% rieb., 1 kg",
+    price: 1.49,
+    glutenFree: false,
+  },
+];
+
+export default products;
